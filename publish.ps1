29c79f2d7275c4ae09ef43e069979a5428415b24
Anthony Howell <anthony@howell-it.com>
2021-12-20 14:16:49 -0800

    adding quiet flag, better output

--- publish.ps1
+++ publish.ps1
@@ -8,13 +8,13 @@ foreach ($manifest in (Get-ChildItem ./ -Filter manifest.txt -Recurse)) {
     # Determine namespace based on folder structure
     $rPath = $manifest.FullName.Replace($baseDir.FullName,'').Trim('\/')
     $namespace = ($rPath -split '\\|\/' | Select-Object -SkipLast 1) -join ':'
+    Write-Host "----------------------------------------------"
     Write-Host "Found: '$rPath', publishing as '$namespace'..."
     
-
     # Publish the Action
     if ($Test.IsPresent) {
-        ./runway.bin -N build -it $manifest.FullName -o $namespace.Replace(':','-')
+        ./runway.bin -q -N build -it $manifest.FullName -o $namespace.Replace(':','-')
     } else {
-        ./runway.bin -N build -it $manifest.FullName -p $namespace.ToLower() -o $namespace.Replace(':','-')
+        ./runway.bin -q -N build -it $manifest.FullName -p $namespace.ToLower() -o $namespace.Replace(':','-')
     }
 }
