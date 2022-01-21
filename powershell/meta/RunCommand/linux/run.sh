90831ca1de46cf8fa43916a08cdf4f0d20db50de
Anthony Howell <anthony@howell-it.com>
2022-01-20 16:00:32 -0800

    adding runcommand

---
+++ powershell/meta/RunCommand/linux/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if ! [ -x "$(command -v pwsh)" ]; then
+    # no powershell installed
+    echo No PowerShell is installed. Unable to continue.
+else
+    pwsh -Executionpolicy Bypass -File ./linux/runCommand.ps1
+fi
