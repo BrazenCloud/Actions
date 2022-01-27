d0a6c466901a5889fda86afcb6bdeaccd0af7ccd
Anthony Howell <anthony@howell-it.com>
2022-01-27 13:47:56 -0800

    fixing file encoding and temp folder

--- secops/CDIR/windows/cdir.ps1
+++ secops/CDIR/windows/cdir.ps1
@@ -3,6 +3,18 @@ Get-ChildItem .\Windows | ForEach-Object {
     Move-Item $_.FullName -Destination .\
 }
 
+# Create a temp folder
+$tempFolder = "C:\TEMP\cdir_$(Get-Date -format yyyyMMdd-hhmmss)"
+if (-not (Test-Path $tempFolder)) {
+    New-Item $tempFolder -ItemType Directory -Force
+}
+
+# Update INI to use the temp folder path
+$ini = Get-Content .\cdir.ini
+$ini = $ini | ?{$_ -notlike 'Output*'}
+$ini += "Output = $($tempFolder)"
+[System.IO.File]::WriteAllLines("$((Get-Location).Path)\cdir.ini", $ini)
+
 $cdir = Get-Item .\cdir-collector.exe
 
 $process = Start-Process $cdir.FullName -RedirectStandardError .\stderr.txt -RedirectStandardOutput .\stdout.txt -PassThru
@@ -19,7 +31,8 @@ if (Test-Path .\stderr.txt) {
             $waiting = $false
         }
     }
-    Write-Host 'Done!'
 } else {
     Write-Host 'No file created. Error suspected.'
 }
+Get-ChildItem $tempFolder | Move-Item -Destination .\results
+Remove-Item $tempFolder -Recurse -Force
--- secops/CDIRwParams/windows/cdir.ps1
+++ secops/CDIRwParams/windows/cdir.ps1
@@ -3,20 +3,27 @@ Get-ChildItem .\Windows | ForEach-Object {
     Move-Item $_.FullName -Destination .\
 }
 
+# Create a temp folder
+$tempFolder = "$($env:TEMP)\cdir_$(Get-Date -format yyyyMMdd-hhmmss)"
+if (-not (Test-Path $tempFolder)) {
+    New-Item $tempFolder -ItemType Directory
+}
+
 # Build the INI
 $settings = Get-Content .\settings.json | ConvertFrom-Json
 $settings
 
 $cdirIni = @{
-    Output = '\results'
+    Output = $tempFolder
 }
 $params = 'MemoryDump','MFT','Secure','UsnJrnl','EventLog','Prefetch','Registry','WMI','SRUM','Web','Target','MemoryDumpCmdline'
 foreach ($param in $params){
     $cdirIni[$param] = $settings.$param
 }
-foreach ($key in $cdirIni.Keys) {
-    "$key = $($cdirIni[$key])`n" | Out-File .\cdir.ini -Append
+$outStr = foreach ($key in $cdirIni.Keys) {
+    "$key = $($cdirIni[$key])"
 }
+[System.IO.File]::WriteAllLines("$((Get-Location).Path)\cdir.ini", $outStr)
 
 # Run the CDIR utility
 $cdir = Get-Item .\cdir-collector.exe
@@ -35,7 +42,9 @@ if (Test-Path .\stderr.txt) {
             $waiting = $false
         }
     }
-    Write-Host 'Done!'
 } else {
     Write-Host 'No file created. Error suspected.'
 }
+
+Get-ChildItem $tempFolder | Move-Item -Destination .\results
+Remove-Item $tempFolder -Recurse -Force
