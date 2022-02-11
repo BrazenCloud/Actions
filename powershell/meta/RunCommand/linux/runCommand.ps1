5a3b154d01d452e2fcae8f9a4808e33498cf55e2
Anthony Howell <anthony@howell-it.com>
2022-02-11 11:46:27 -0800

    adding in default props only param

--- powershell/meta/RunCommand/linux/runCommand.ps1
+++ powershell/meta/RunCommand/linux/runCommand.ps1
@@ -16,5 +16,16 @@ $sb = [scriptblock]::Create($settings.Command)
 if ($settings.'Raw Output'.ToString() -eq 'true') {
     Invoke-Command -ScriptBlock $sb
 } else {
+    $output = Invoke-Command -ScriptBlock $sb
+    $select = '*'
+    if ($settings.'Default Properties Only' -eq $true) {
+        if ($output.GetType().BaseType.Name -eq 'Array') {
+            if ($output[0].PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames) {
+                $select = $output[0].PSStandardMembers.DefaultDisplayPropertySet.ReferencedPropertyNames
+            }
+        }
+    }
+    $output | Select-Object $select | Export-Clixml -Depth $settings.'Serialize Depth' .\results\output.clixml #ConvertTo-Xml -As String -Depth $settings.'Serialize Depth'
+    Get-Content .\results\output.clixml
     Invoke-Command -ScriptBlock $sb | ConvertTo-Xml -As String -Depth $settings.'Serialize Depth'
 }
