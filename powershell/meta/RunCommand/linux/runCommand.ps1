4ec491d792e11b89a35e400f28fa6f48d9e620ba
Anthony Howell <anthony@howell-it.com>
2022-02-11 11:27:14 -0800

    adding additional options

--- powershell/meta/RunCommand/linux/runCommand.ps1
+++ powershell/meta/RunCommand/linux/runCommand.ps1
@@ -1,3 +1,20 @@
-$settings = Get-Content .\settings.json | ConvertFrom-Json
+# Load Settings
+$json = Get-Content .\settings.json
+$settings = $json | ConvertFrom-Json
+
+if ($settings.Debug.ToString() -eq 'Debug') {
+    $json.Split("`n") | Foreach-Object {"# $_"}
+}
+
+# Output PS version (debugging)
+if ($settings.Debug.ToString() -eq 'Debug') {
+    "# $($PSVersionTable.PSVersion.ToString())"
+}
+
+# Execute command
 $sb = [scriptblock]::Create($settings.Command)
-Invoke-Command $sb | ConvertTo-Xml -As String -Depth $settings.'Serialize Depth'
+if ($settings.'Raw Output'.ToString() -eq 'true') {
+    Invoke-Command -ScriptBlock $sb
+} else {
+    Invoke-Command -ScriptBlock $sb | ConvertTo-Xml -As String -Depth $settings.'Serialize Depth'
+}
