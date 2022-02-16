7ff845977e155890fb050e73cf8c0d419f848310
Anthony Howell <anthony@howell-it.com>
2022-02-16 09:21:12 -0800

    adding main readme TOC

---
+++ reposcripts/buildTOC.ps1
@@ -0,0 +1,53 @@
+$manifests = Get-ChildItem .\ -Filter manifest.txt -Recurse
+$actions = foreach ($manifest in $manifests) {
+    $rPath = $manifest.FullName.Replace($baseDir.FullName,'').Trim('\/')
+    $actionName = ($rPath -split '\\|\/' | Select-Object -SkipLast 1) -join ':'
+    [pscustomobject]@{
+        Name = ($rPath -split '\\|\/' | Select-Object -SkipLast 1) -join ':'
+        RelativePath = $rPath.Replace('\manifest.txt','')
+        Parent = $rPath.Split('\')[0]
+    }
+}
+
+$newContent = foreach ($parent in $actions.Parent | Select -Unique) {
+    "- $parent"
+    foreach ($action in $actions | ?{$_.Parent -eq $parent}) {
+        "  - [$($action.Name)]($($action.RelativePath))"
+    }
+}
+
+
+$preLines = if (Test-Path .\README.md) {
+    $content = Get-Content .\README.md
+    $capture = $true
+    :lines foreach ($line in $content) {
+        if ($line -like '<!-- region *') {
+            break lines
+        } elseif ($capture) {
+            $line
+        }
+    }
+}
+
+$postLines = if (Test-Path .\README.md) {
+    $content = Get-Content .\README.md
+    $capture = $false
+    :lines foreach ($line in $content) {
+        if ($line -like '<!-- endregion -->*') {
+            $capture = $true
+        } elseif ($capture) {
+            $line
+        }
+    }
+}
+
+Remove-Item .\README.md
+if ($null -ne $preLines) {
+    $preLines | Out-File .\README.md -Append
+}
+'<!-- region Generated -->' | Out-File .\README.md -Append
+$newContent | Out-File .\README.md -Append
+'<!-- endregion -->*' | Out-File .\README.md -Append
+if ($null -ne $postLines) {
+    $postLines | Out-File .\README.md -Append
+}
