d5760bf34edd42a55822ae9aca2e3ada6101c34e
Anthony Howell <anthony@howell-it.com>
2022-02-17 14:56:45 -0800

    extending the json output

--- ad/users/getUser/windows/script.ps1
+++ ad/users/getUser/windows/script.ps1
@@ -8,7 +8,10 @@ if ((Get-Module 'ActiveDirectory' -ListAvailable).Count -ge 1) {
     if ($settings.'Additional Properties') {
         $splat['Properties'] = $settings.'Additional Properties'
     }
-    Get-AdUser @splat
+    $out = Get-AdUser @splat
+    $out
+    $out | ConvertTo-Json
+    $out | ConvertTo-Json | Out-File .\results\users.json
 } else {
     Write-Host 'ActiveDirectory module is not installed.'
 }
