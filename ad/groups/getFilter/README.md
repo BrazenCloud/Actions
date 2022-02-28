f4d0d7e60b499572205be0da10d4d8eeec4b4db5
Anthony Howell <anthony@howell-it.com>
2022-02-28 08:37:22 -0800

    adding in group actions

---
+++ ad/groups/getFilter/README.md
@@ -0,0 +1,36 @@
+<!-- region Generated -->
+# ad:groups:getFilter
+
+Returns groups from Active Directory
+
+## Metadata
+
+- Tags:
+  - Windows
+  - PowerShell
+  - ActiveDirectory
+  - Group
+- Language: PowerShell
+- Supported Operating Systems:
+  - Windows
+
+## Parameters
+
+- Filter
+  - Description: The filter used to return groups from AD.
+  - Type: String
+  - IsOptional: False
+  - DefaultValue: *
+- Additional properties
+  - Description: Any additional properties, seperated by commas.
+  - Type: String
+  - IsOptional: True
+- Only JSON
+  - Description: Will only write JSON to stdout
+  - Type: Boolean
+  - IsOptional: True
+- CSV Out
+  - Description: Will write the results as a CSV
+  - Type: Boolean
+  - IsOptional: True
+<!-- endregion -->
---
+++ ad/groups/getFilter/windows/script.ps1
@@ -0,0 +1,26 @@
+$defaultComputerProperties = @('DistinguishedName','GroupCategory','GroupScope',
+'Name','ObjectClass','ObjectGUID','SamAccountName','SID')
+
+$settings = Get-Content .\settings.json | ConvertFrom-Json
+
+if ((Get-Module 'ActiveDirectory' -ListAvailable).Count -ge 1) {
+    $splat = @{
+        Filter = $settings.Filter
+    }
+    if ($settings.'Additional Properties') {
+        $defaultComputerProperties = $defaultComputerProperties + $settings.'Additional Properties'.Split(',') | Select-Object -Unique
+        $splat['Properties'] = $settings.'Additional Properties'.Split(',')
+    }
+    $out = Get-AdComputer @splat
+    if ($settings.'Only JSON'.ToString() -ne 'true') {
+        $out
+    }
+    $out | Select-Object $defaultComputerProperties | ConvertTo-Json -Depth 1
+    if ($settings.'CSV Out'.ToString() -eq 'true') {
+        $out | Select-Object $defaultComputerProperties | Export-Csv .\results\computers.csv -NoTypeInformation
+    } else {
+        $out | Select-Object $defaultComputerProperties | ConvertTo-Json -Depth 1 | Out-File .\results\computers.json
+    }
+} else {
+    Write-Host 'ActiveDirectory module is not installed.'
+}
