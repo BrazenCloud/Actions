ff0aebd30a3ad296b226f8505934e71bf46c01a9
Anthony Howell <anthony@howell-it.com>
2022-02-28 10:19:09 -0800

    removing parameter

--- ad/groups/removeMember/windows/script.ps1
+++ ad/groups/removeMember/windows/script.ps1
@@ -5,7 +5,7 @@ if ((Get-Module 'ActiveDirectory' -ListAvailable).Count -ge 1) {
         Identity = $settings.Group
         Members = $settings.Members.Split(',')
     }
-    Remove-AdGroupMember @splat -Confirm:$false -Force
+    Remove-AdGroupMember @splat -Confirm:$false
 } else {
     Write-Host 'ActiveDirectory module is not installed.'
 }
