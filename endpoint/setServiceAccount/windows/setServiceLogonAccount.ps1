58aa15db26404152662a15694e0706c4502f4556
Anthony Howell <anthony@howell-it.com>
2022-03-16 11:09:40 -0700

    adding setServiceLogonAccount

---
+++ endpoint/setServiceAccount/windows/setServiceLogonAccount.ps1
@@ -0,0 +1,69 @@
+Function Add-LogonAsAServiceUser {
+    param (
+        [string]$UserName
+    )
+    # Find the user SID
+    $sid = [System.Security.Principal.NTAccount]::new($username).Translate([System.Security.Principal.SecurityIdentifier]).Value
+
+    # Clean up files, if they exist
+    $files = 'import.inf','export.inf','secedt.sdb'
+    foreach ($file in $files) {
+        if (Test-Path .\$file) {
+            Remove-Item .\$file -Force
+        }
+    }
+
+    # Export the settings
+    secedit /export /cfg .\export.inf
+
+    # Find the existing logon as a service accounts
+    $existingSids = (Select-String .\export.inf -Pattern "SeServiceLogonRight").Line
+
+    # Build the update file
+    $updateStr = @"
+[Unicode]
+Unicode=yes
+[System Access]
+[Event Audit]
+[Registry Values]
+[Version]
+signature="`$CHICAGO$"
+Revision=1
+[Profile Description]
+Description=GrantLogOnAsAService security template
+[Privilege Rights]
+$existingSids,*$sid
+"@
+    $updateStr | Out-File .\import.inf
+
+    secedit /import /db .\secedt.sdb /cfg .\import.inf
+    secedit /configure /db .\secedt.sdb
+    #gpupdate /Target:Computer
+    foreach ($file in $files) {
+        Remove-Item .\$file -Force
+    }
+}
+
+$settings = Get-Content .\settings.json | ConvertFrom-Json
+$settings
+
+$settings | Select * -ExcludeProperty 'User Password' | ConvertTo-Json | Out-File .\settings.json -Force
+
+<#
+PowerShell 7:
+$ssPw = ConvertTo-SecureString $settings.'User Password' -AsPlainText -Force
+$credential = [pscredential]::New($settings.'User Name',$ssPw)
+
+Set-Service -Name $settings.'Service Name' -Credential $credential -Verbose
+#>
+
+# Windows PowerShell
+
+# Allow the user to logon as a service
+Add-LogonAsAServiceUser -UserName $settings.'User Name'
+
+# Update the service
+sc.exe config `"$($settings.'Service Name')`" obj=`"$($settings.'User Name')`" password=`"$($settings.'User Password')`"
+
+# Restart the service
+Restart-Service $settings.'Service Name'
