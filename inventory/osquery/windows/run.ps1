0ba415a0f88bebb201da5989a32194a79a0addb0
Anthony Howell <anthony@howell-it.com>
2022-05-19 07:37:57 -0700

    adjusting osquery to support elastic

--- inventory/osquery/windows/run.ps1
+++ inventory/osquery/windows/run.ps1
@@ -1,17 +1,30 @@
+# Load settings
+$settings = Get-Content .\settings.json | ConvertFrom-Json 
 
-#
-# change the CWD to the script directory
-#
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
-#Write-host "My directory is $dir"
-Set-Location -Path $dir
-
-#
-# read the user configured data from settings
-#
-$settings = Get-Content ..\settings.json | ConvertFrom-Json 
+# Debug
 $search = $settings.Query
-Write-host "my search is $search"
+Write-host "Running query: $search"
 
-.\osqueryi.exe "$search" >> ..\results\query_results.txt
+$queryResults = .\windows\osqueryi.exe "$search" --json
+
+# if formatting for elastic, use the filebeat osquery module format
+if ($settings.'Format for Elastic'.ToString() -eq 'true') {
+    $out = @{
+        osquery = @{
+            results = @{
+                name           = $settings.'Query Name'
+                hostIdentifier = hostname
+                CalendarTime   = (Get-Date -UFormat "%a %b %d %T %Y")
+                unixTime       = (Get-Date -UFormat %s)
+                epoch          = 0
+                counter        = 0
+                numerics       = $false
+                columns        = $queryResults | ConvertFrom-Json
+                action         = 'added'
+            }
+        }
+    }
+    $out | ConvertTo-Json -Depth 4 | Out-File .\results\osquery.json
+} else {
+    $queryResults | Out-File .\results\osquery.json
+}
