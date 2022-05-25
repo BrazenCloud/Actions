f5a32b3eb8ee113b38c5412ac764b2745b553618
Anthony Howell <anthony@howell-it.com>
2022-05-25 11:46:10 -0700

    adding additional staging support

--- reposcripts/publish.ps1
+++ reposcripts/publish.ps1
@@ -1,20 +1,21 @@
 param (
-    [switch]$Test
+    [switch]$Test,
+    [string]$Server = 'portal.runway.host'
 )
 
 $baseDir = Get-Item ./
 foreach ($manifest in (Get-ChildItem ./ -Filter manifest.txt -Recurse)) {
     
     # Determine namespace based on folder structure
-    $rPath = $manifest.FullName.Replace($baseDir.FullName,'').Trim('\/')
+    $rPath = $manifest.FullName.Replace($baseDir.FullName, '').Trim('\/')
     $namespace = ($rPath -split '\\|\/' | Select-Object -SkipLast 1) -join ':'
     Write-Host "----------------------------------------------"
     Write-Host "Found: '$rPath', publishing as '$namespace'..."
     
     # Publish the Action
     if ($Test.IsPresent) {
-        ./runway.bin -q -N build -i $($manifest.FullName) -o "$($namespace.Replace(':','-')).apt"
+        ./runway.bin -q -N -S $Server build -i $($manifest.FullName) -o "$($namespace.Replace(':','-')).apt"
     } else {
-        & ./runway.bin -q -N build -i $($manifest.FullName) -o "$($namespace.Replace(':','-')).apt" -p $($namespace.ToLower()) --PUBLIC
+        & ./runway.bin -q -N -S $Server build -i $($manifest.FullName) -o "$($namespace.Replace(':','-')).apt" -p $($namespace.ToLower()) --PUBLIC
     }
 }
