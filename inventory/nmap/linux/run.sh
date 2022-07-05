7f9cc7f3287fffd2cc367b5b660c74fbc8151f90
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-05 15:57:05 -0500

    fix syntax of results filename

--- inventory/nmap/linux/run.sh
+++ inventory/nmap/linux/run.sh
@@ -4,5 +4,5 @@ sudo apt-get install nmap -y
 apt-get install nmap -y
 
 # write nscd cache stats to dns_cache
-nmap  127.0.0.1 --p 0-65535 >> ../results/masscan.txt
+nmap  127.0.0.1 --p 0-65535 >> ../results/nmapscan.txt
 
