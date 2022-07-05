e9e422b1f9709c81e61d33a8b367dabe2221d465
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-05 16:36:53 -0500

    update location of settings.json

--- inventory/nmap/linux/run.sh
+++ inventory/nmap/linux/run.sh
@@ -5,8 +5,8 @@ apt-get install nmap -y
 sudo apt-get install jq -y
 apt-get install jq -y
 
-portstoscan1=$(jq '."Ports to scan"' settings.json)
-subnet1=$(jq '."Network Subnet"' settings.json)
+portstoscan1=$(jq '."Ports to scan"' ../settings.json)
+subnet1=$(jq '."Network Subnet"' ../settings.json)
 
 # write nscd cache stats to dns_cache
 nmap $subnet1 $portstoscan1 >> ../results/nmapscan.txt
