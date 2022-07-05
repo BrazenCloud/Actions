017fce8d1abd3e8b5b9e6c54eecedd704d096dd3
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-05 16:30:30 -0500

    update nmap action with parameter passing

--- inventory/nmap/linux/run.sh
+++ inventory/nmap/linux/run.sh
@@ -2,7 +2,12 @@ cd "${0%/*}"
 
 sudo apt-get install nmap -y
 apt-get install nmap -y
+sudo apt-get install jq -y
+apt-get install jq -y
+
+portstoscan1=$(jq '."Ports to scan"' settings.json)
+subnet1=$(jq '."Network Subnet"' settings.json)
 
 # write nscd cache stats to dns_cache
-nmap  127.0.0.1 -p 0-65535 >> ../results/nmapscan.txt
+nmap $subnet1 $portstoscan1 >> ../results/nmapscan.txt
 
