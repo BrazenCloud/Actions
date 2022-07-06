0adb95a38e046cce50f2d2b8f545e8bb84d2021e
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-05 19:33:57 -0500

    update debsums with yum install ahead of execution for centos

--- secops/debsums/linux/run.sh
+++ secops/debsums/linux/run.sh
@@ -3,6 +3,8 @@ cd "${0%/*}"
 sudo apt-get install debsums -y
 apt-get install debsums -y
 sudo apt install debsums -y
+sudo yum install debsums -y
+yum install debsums -y
 
 # examplevar1=$(jq -r '."Ports to scan"' ../settings.json)
 # examplevar1=$(jq -r '."Network Subnet"' ../settings.json)
