93024902f997b09de63be7b874b4c07af5362740
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-05 19:18:53 -0500

    create a new debsums action to validate packages (silent mode)

---
+++ secops/debsums/linux/run.sh
@@ -0,0 +1,12 @@
+cd "${0%/*}"
+
+sudo apt-get install debsums -y
+apt-get install debsums -y
+sudo apt install debsums -y
+
+# examplevar1=$(jq -r '."Ports to scan"' ../settings.json)
+# examplevar1=$(jq -r '."Network Subnet"' ../settings.json)
+
+# write nscd cache stats to dns_cache
+debsums -s >> ../results/debsums.txt
+
