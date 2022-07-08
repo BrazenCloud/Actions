6cc6aa7ebd5789c139d602f25786f66ff400fc2b
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-07 19:32:40 -0500

    Add send rate to nmap parameters

--- inventory/nmap/linux/run.sh
+++ inventory/nmap/linux/run.sh
@@ -7,7 +7,8 @@ apt-get install jq -y
 
 portstoscan1=$(jq -r '."Ports to scan"' ../settings.json)
 subnet1=$(jq -r '."Network Subnet"' ../settings.json)
+maxratepps1=$(jq -r '."Network Subnet"' ../settings.json)
 
 # write nscd cache stats to dns_cache
-nmap $subnet1 $portstoscan1 >> ../results/nmapscan.txt
+nmap $subnet1 $portstoscan1 $maxratepps1 >> ../results/nmapscan.txt
 
