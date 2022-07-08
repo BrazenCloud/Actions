588ee34d19692f806350893ca8431046b9281ca3
Anthony Howell <anthony@howell-it.com>
2022-07-08 08:22:07 -0700

    small improvements

--- secops/radare2/windows/script.ps1
+++ secops/radare2/windows/script.ps1
@@ -6,7 +6,7 @@ if ($settings.'Add Strings Output'.ToString() -eq 'true') {
     $command = "$command -qc izz"
 }
 
-if ($settings.'Output as json') {
+if ($settings.'Output as json'.ToString() -eq 'true') {
     $commandsplit = $command.Split(' ')
     for ($x = 0; $x -lt $commandsplit.count; $x++) {
         if ($commandsplit[$x].Trim() -eq '-qc') {
@@ -19,7 +19,10 @@ if ($settings.'Output as json') {
 }
 
 if (Test-Path $settings.'Scanned file filename and path') {
-    $sb = [scriptblock]::Create("$command $($settings.'Scanned file filename and path')")
+    $path = (Resolve-Path $settings.'Scanned file filename and path').Path
+    $sb = [scriptblock]::Create("$command '$path'")
+
+    Write-Host "Running command: $sb"
     Invoke-Command $sb
 } else {
     Throw "'$($settings.'Scanned file filename and path')' is not a valid path."
