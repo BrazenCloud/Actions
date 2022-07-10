a7f3f1b61b779649a1b906fabe0c1c82acee8c2b
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-10 17:03:45 -0500

    Add raccoon

---
+++ secops/raccoon/linux/run.sh
@@ -0,0 +1,23 @@
+cd "${0%/*}"
+
+pythonCMD="python"
+
+if ! [ -x "$(command -v python)" ]; then
+    # no python installed
+    pythonCMD="./python3.7"
+    chmod +x $pythonCMD 
+fi
+
+$pythonCMD
+
+sudo apt-get install jq -y
+sudo apt-get install pip -y
+pip install raccoon-scanner -y
+
+hosttoscan1=$(jq -r '."hosttoscan"' ../settings.json)
+verbose1=$(jq -r '."quietmode"' ../settings.json)
+#examplevar=$(jq -r '."Example"' ../settings.json)
+
+# Execute intelspy
+raccoon $verbose1 -f --vulners-nmap-scan $hosttoscan1 -o ../results
+
