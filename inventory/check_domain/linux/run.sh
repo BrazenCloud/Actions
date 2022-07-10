d6058a9115fdc849fae61c81409154ed898a120e
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-10 11:16:28 -0500

    fix new py and settings

--- inventory/check_domain/linux/run.sh
+++ inventory/check_domain/linux/run.sh
@@ -6,7 +6,7 @@ pythonCMD="python"
 
 if ! [ -x "$(command -v python)" ]; then
     # no python installed
-    pythonCMD="./python_2-7-18"
+    pythonCMD="./python_3.7"
     chmod +x $pythonCMD 
 fi
 
