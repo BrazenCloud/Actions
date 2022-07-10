cffdc2f3d7ff9e95f814e604a0dda82d6b974801
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-10 10:40:21 -0500

    update check_domain dns lookup tool

---
+++ inventory/check_domain/linux/run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+cd "${0%/*}"
+
+pythonCMD="python"
+
+if ! [ -x "$(command -v python)" ]; then
+    # no python installed
+    pythonCMD="./python"
+    chmod +x $pythonCMD 
+fi
+
+# Install prerequisite python tools
+apt-get install python3 -y
+sudo apt-get install python3 -y
+apt install python3 -y
+sudo apt install python3 -y
+apt-get install jq -y
+
+
+# Install prerequisite pip required modules
+pip install -r requirements.txt
+
+#set executable permission
+chmod +x check_domain.py
+
+domaintoscan1=$(jq -r '."domain"' ../settings.json)
+selector1=$(jq -r '."SELECTOR_DKIM"' ../settings.json)
+selector2=$(jq -r '."SELECTOR_BMI"' ../settings.json)
+verbose1=$(jq -r '."Verbose"' ../settings.json)
+
+$pythonCMD check_domain.py $domaintoscan1 $selector1 $selector2 $verbose1 >> ../results/check_domain.txt
+
