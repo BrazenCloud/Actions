cce1e1b30e5ccd26a0c4fa2f801c3e37c6801188
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-10 18:20:22 -0500

    save scan output to hostnamedir

--- secops/raccoon/linux/run.sh
+++ secops/raccoon/linux/run.sh
@@ -26,4 +26,4 @@ quietmode1=$(jq -r '."quietmode1"' ../settings.json)
 #examplevar=$(jq -r '."Example"' ../settings.json)
 
 # Execute raccooon
-raccoon -f $hosttoscan1 --outdir ../results/raccoon.txt
+raccoon -f $hosttoscan1 --outdir ../results/$hosttoscan1
