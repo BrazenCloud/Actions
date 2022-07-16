3d498f3c0278d5ea06e7f5ec28c27faa43e3c0d1
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-15 19:46:18 -0500

    fix nmap speed

--- inventory/nmap/linux/run.sh
+++ inventory/nmap/linux/run.sh
@@ -7,7 +7,7 @@ apt-get install jq -y
 
 portstoscan1=$(jq -r '."Ports to scan"' ../settings.json)
 subnet1=$(jq -r '."Network Subnet"' ../settings.json)
-maxratepps1=$(jq -r '."Network Subnet"' ../settings.json)
+maxratepps1=$(jq -r '."Max rate packets per second"' ../settings.json)
 
 # write nscd cache stats to dns_cache
 nmap $subnet1 $portstoscan1 $maxratepps1 >> ../results/nmapscan.txt
