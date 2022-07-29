4791f1b9a6dbdb88d2ac0a8f2e8ee06442c1bac4
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-28 19:13:38 -0500

    Fixing package detection nmap

--- inventory/nmap/linux/run.sh
+++ inventory/nmap/linux/run.sh
@@ -1,9 +1,22 @@
 cd "${0%/*}"
 
-sudo apt-get install nmap -y
-apt-get install nmap -y
-sudo apt-get install jq -y
-apt-get install jq -y
+REQUIRED_PKG1="nmap"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG1|grep "install ok installed")
+echo Checking for $REQUIRED_PKG1: $PKG1_OK
+if [ "" = "$PKG1_OK" ]; then
+  echo "No $REQUIRED_PKG1. Setting up $REQUIRED_PKG1."
+  sudo apt-get --yes install $REQUIRED_PKG1
+  apt-get --yes install $REQUIRED_PKG1 
+fi
+
+REQUIRED_PKG2="jq"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG2|grep "install ok installed")
+echo Checking for $REQUIRED_PKG2: $PKG2_OK
+if [ "" = "$PKG2_OK" ]; then
+  echo "No $REQUIRED_PKG2. Setting up $REQUIRED_PKG2."
+  sudo apt-get --yes install $REQUIRED_PKG2 
+  apt-get --yes install $REQUIRED_PKG2
+fi
 
 portstoscan1=$(jq -r '."Ports to scan"' ../settings.json)
 subnet1=$(jq -r '."Network Subnet"' ../settings.json)
