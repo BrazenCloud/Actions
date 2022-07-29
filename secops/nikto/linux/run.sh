843dcd5b8f1b56f2279e9e16c08da5dcc0c7b0bf
Lawrence Pingree <geekguy@geek-guy.com>
2022-07-28 20:01:34 -0500

    check packages in nikto

--- secops/nikto/linux/run.sh
+++ secops/nikto/linux/run.sh
@@ -1,9 +1,13 @@
 cd "${0%/*}"
+IRED_PKGS="perl libnet-ssleay-perl openssl libauthen-pam-perl libio-pty-perl jq"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG2|grep "install ok installed")
+echo Checking for $REQUIRED_PKGS: $PKGS_OK
+if [ "" = "$PKG2_OK" ]; then
+  echo "No $REQUIRED_PKGS. Setting up $REQUIRED_PKGS."
+  sudo apt-get --yes install $REQUIRED_PKGS 
+  apt-get --yes install $REQUIRED_PKGS
+fi
 
-sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libio-pty-perl -y > /dev/null
-apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libio-pty-perl -y > /dev/null
-sudo apt-get install jq -y > /dev/null
-apt-get install jq -y > /dev/null
 chmod +x ./nikto.pl
 
 hosttoscan1=$(jq -r '."hosttoscan1"' ../settings.json)
