b72d0aebc73d096b969557472dd754b8e33ce7a8
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-02 18:17:45 -0500

    fix chkrootkit

--- secops/chkrootkit/linux/run.sh
+++ secops/chkrootkit/linux/run.sh
@@ -1,15 +1,15 @@
 cd "${0%/*}"
 
-REQUIRED_PKG2="jq apt-utils"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG2|grep "install ok installed")
-echo Checking for $REQUIRED_PKG2: $PKG2_OK
-if [ "" = "$PKG2_OK" ]; then
-  echo "No $REQUIRED_PKG2. Setting up $REQUIRED_PKG2."
-  sudo apt-get --yes install $REQUIRED_PKG2 
-  apt-get --yes install $REQUIRED_PKG2
+REQUIRED_PKGS="jq apt-utils"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS|grep "install ok installed")
+echo Checking for $REQUIRED_PKGS: $PKGS_OK
+if [ "" = "$PKGS_OK" ]; then
+  echo "No $REQUIRED_PKGS. Setting up $REQUIRED_PKGS."
+  sudo apt-get --yes install $REQUIRED_PKGS 
+  apt-get --yes install $REQUIRED_PKGS
 fi
 
 
 params=$(jq -r '."Parameters"' ../settings.json)
-chmod +x ./chkrootkit
-chkrootkit $params
+chmod +x ../chkrootkit
+../chkrootkit $params
