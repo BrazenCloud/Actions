e21cfd9cbd135affcf3456adeab3c79c5c1aa020
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-02 09:40:36 -0500

    fix package install

--- secops/chkrootkit/linux/run.sh
+++ secops/chkrootkit/linux/run.sh
@@ -2,8 +2,15 @@
 
 cd "${0%/*}"
 
-sudo apt-get install jq -y  > /dev/null
-apt-get install jq -y  > /dev/null
+REQUIRED_PKG2="jq"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG2|grep "install ok installed")
+echo Checking for $REQUIRED_PKG2: $PKG2_OK
+if [ "" = "$PKG2_OK" ]; then
+  echo "No $REQUIRED_PKG2. Setting up $REQUIRED_PKG2."
+  sudo apt-get --yes install $REQUIRED_PKG2 
+  apt-get --yes install $REQUIRED_PKG2
+fi
+
 
 params=$(jq -r '."Parameters"' ../settings.json)
 
