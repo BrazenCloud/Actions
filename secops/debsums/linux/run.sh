4f932fd5f9a609b78cb7ed57f0d3bddd39be2918
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-02 09:47:26 -0500

    fix debsums

--- secops/debsums/linux/run.sh
+++ secops/debsums/linux/run.sh
@@ -1,15 +1,6 @@
 cd "${0%/*}"
 
-REQUIRED_PKG2="debsums"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG2|grep "install ok installed")
-echo Checking for $REQUIRED_PKG2: $PKG2_OK
-if [ "" = "$PKG2_OK" ]; then
-  echo "No $REQUIRED_PKG2. Setting up $REQUIRED_PKG2."
-  sudo apt-get --yes install $REQUIRED_PKG2 
-  apt-get --yes install $REQUIRED_PKG2
-fi
-
-REQUIRED_PKG2="jq"
+REQUIRED_PKG2="debsums jq"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG2|grep "install ok installed")
 echo Checking for $REQUIRED_PKG2: $PKG2_OK
 if [ "" = "$PKG2_OK" ]; then
