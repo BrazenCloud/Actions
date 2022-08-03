2262a31de9b2bd72525f22faf2fbaf90af38e2bf
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-02 19:45:10 -0500

    fix chkrootkit

--- secops/chkrootkit/linux/run.sh
+++ secops/chkrootkit/linux/run.sh
@@ -2,7 +2,7 @@
 
 cd "${0%/*}"
 
-REQUIRED_PKGS="jq apt-utils"
+REQUIRED_PKGS="jq apt-utils binutils"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS|grep "install ok installed")
 echo Checking for $REQUIRED_PKGS: $PKGS_OK
 if [ "" = "$PKGS_OK" ]; then
