8300def55d1aa819fd36408269545b099188e3e8
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-03 07:25:22 -0500

    Fix chkrootkit

--- secops/chkrootkit/linux/run.sh
+++ secops/chkrootkit/linux/run.sh
@@ -13,5 +13,5 @@ if [ "" = "$PKGS_OK" ]; then
 fi
 
 params=$(jq -r '."Parameters"' ../settings.json)
-chmod +x ./chkrootkit
-./chkrootkit $params >> ../results/chkrootkit.txt
+chmod +X ./chkrootkit
+chkrootkit $params >> ../results/chkrootkit.txt
