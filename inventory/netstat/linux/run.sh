6483a3f43e7495dccdc48d0f699205b27839a7c7
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-03 15:23:55 -0500

    Fix netstat package install dependency

--- inventory/netstat/linux/run.sh
+++ inventory/netstat/linux/run.sh
@@ -2,6 +2,15 @@
 
 cd "${0%/*}"
 
+REQUIRED_PKG1="net-tools"
+PKG_OK1=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG1|grep "install ok installed")
+echo Checking for $REQUIRED_PKG1: $PKG1_OK
+if [ "" = "$PKG1_OK" ]; then
+  echo "No $REQUIRED_PKG1. Setting up $REQUIRED_PKG1."
+  sudo apt-get --yes install $REQUIRED_PKG1
+  apt-get --yes install $REQUIRED_PKG1 
+fi
+
 pythonCMD="python"
 
 if ! [ -x "$(command -v python)" ]; then
