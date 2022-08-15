56458196989e91bdb0182a233dc58601f8548803
Lawrence Pingree <geekguy@geek-guy.com>
2022-08-15 11:28:16 -0500

    Fix install sequence

--- secops/fierce/linux/run.sh
+++ secops/fierce/linux/run.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 cd "${0%/*}"
 
-REQUIRED_PKGS="jq pip whois fierce python3"
-PKGS_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS|grep "install ok installed")
-echo Checking for $REQUIRED_PKGS: $PKGS_OK
-if [ "" = "$PKGS_OK" ]; then
-  echo "No $REQUIRED_PKGS. Setting up $REQUIRED_PKGS."
-  sudo apt-get --yes install $REQUIRED_PKGS 
-  apt-get --yes install $REQUIRED_PKGS
+REQUIRED_PKGS1="jq"
+PKGS1_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS1|grep "install ok installed")
+echo Checking for $REQUIRED_PKGS1: $PKGS1_OK
+if [ "" = "$PKGS1_OK" ]; then
+  echo "No $REQUIRED_PKGS1. Setting up $REQUIRED_PKGS1."
+  sudo apt-get --yes install $REQUIRED_PKGS1 
+  apt-get --yes install $REQUIRED_PKGS1
 fi
-pip install fierce  > /dev/null
 
+REQUIRED_PKGS4="python3"
+PKGS4_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS4|grep "install ok installed")
+echo Checking for $REQUIRED_PKGS4: $PKGS4_OK
+if [ "" = "$PKGS4_OK" ]; then
+  echo "No $REQUIRED_PKGS4. Setting up $REQUIRED_PKGS4."
+  sudo apt-get --yes install $REQUIRED_PKGS4 
+  apt-get --yes install $REQUIRED_PKGS4
+fi
+
+REQUIRED_PKGS2="pip"
+PKGS2_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS2|grep "install ok installed")
+echo Checking for $REQUIRED_PKG2S: $PKGS2_OK
+if [ "" = "$PKGS2_OK" ]; then
+  echo "No $REQUIRED_PKGS2. Setting up $REQUIRED_PKGS2."
+  sudo apt-get --yes install $REQUIRED_PKGS2 
+  apt-get --yes install $REQUIRED_PKGS2
+fi
+
+REQUIRED_PKGS3="whois"
+PKGS3_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS3|grep "install ok installed")
+echo Checking for $REQUIRED_PKGS: $PKGS3_OK
+if [ "" = "$PKGS3_OK" ]; then
+  echo "No $REQUIRED_PKGS3. Setting up $REQUIRED_PKGS3."
+  sudo apt-get --yes install $REQUIRED_PKGS3
+  apt-get --yes install $REQUIRED_PKGS3
+fi
+
+pip install fierce  > /dev/null
 
 
 hosttoscan=$(jq -r '."hosttoscan"' ../settings.json)
