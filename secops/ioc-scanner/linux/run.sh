a71a2cb686267d9c37477f76364508dd4ab460d6
Lawrence Pingree <geekguy@geek-guy.com>
2022-09-21 15:14:34 -0500

    ioc-scanner updates

--- secops/ioc-scanner/linux/run.sh
+++ secops/ioc-scanner/linux/run.sh
@@ -3,9 +3,9 @@ cd "${0%/*}"
 
 #subnet1=$(jq -r '."Network Subnet"' ../settings.json)
 #maxratepps1=$(jq -r '."Network Subnet"' ../settings.json)
-chmod +x ./ioc-scanner
+chmod +x ioc-scanner
 echo $Hash >> hashfile.txt
 
 # write nscd cache stats to dns_cache
-./ioc-scanner -f ./hashfile.txt >> ../results/ioc-scanner.txt
+ioc-scanner -f ./hashfile.txt >> ../results/ioc-scanner.txt
 
