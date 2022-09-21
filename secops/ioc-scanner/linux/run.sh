0d775772a8cba37da635f23fdf36f56b069f6659
Lawrence Pingree <geekguy@geek-guy.com>
2022-09-21 15:35:28 -0500

    fix run script

--- secops/ioc-scanner/linux/run.sh
+++ secops/ioc-scanner/linux/run.sh
@@ -1,6 +1,15 @@
 cd "${0%/*}"
 
+REQIRED_PKGS="jq"
+PKGS_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKGS|grep "install ok installed")
+echo Checking for $REQUIRED_PKGS: $PKGS_OK
+if [ "" = "$PKGS_OK" ]; then
+  echo "No $REQUIRED_PKGS. Setting up $REQUIRED_PKGS."
+  sudo apt-get --yes install $REQUIRED_PKGS 
+  apt-get --yes install $REQUIRED_PKGS
+fi
 
+Hash=$(jq -r '."Hash"' ../settings.json)
 #subnet1=$(jq -r '."Network Subnet"' ../settings.json)
 #maxratepps1=$(jq -r '."Network Subnet"' ../settings.json)
 chmod +x ./ioc_scanner
