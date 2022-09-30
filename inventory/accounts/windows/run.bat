0e9b3b53dc46a7ee73c87eab1fa5e2cfee0a7e1b
Anthony Howell <anthony@howell-it.com>
2022-09-30 13:14:33 -0700

    fixing account inventory on Windows

--- inventory/accounts/windows/run.bat
+++ inventory/accounts/windows/run.bat
@@ -1,5 +1,5 @@
-net user > ..\Results\users.txt
-net group "administrators" > ..\Results\admins.txt
-net localgroup administrators >> ..\Results\admins.txt
-wmic useraccount get /all /format:list >> ..\Results\users.txt
-wmic useraccount list full >> ..\Results\users.txt
+net user > .\Results\users.txt
+net group "administrators" > .\Results\admins.txt
+net localgroup administrators >> .\Results\admins.txt
+wmic useraccount get /all /format:list >> .\Results\users.txt
+wmic useraccount list full >> .\Results\users.txt
