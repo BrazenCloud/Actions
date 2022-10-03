03cf96b061cafefa9acf85dcb4168934ef937d09
Anthony Howell <anthony@howell-it.com>
2022-10-03 14:56:53 -0700

    adding linux support for connect/listen

--- runway/stream/connect/linux/run.sh
+++ runway/stream/connect/linux/run.sh
@@ -1,3 +1,34 @@
+#!/bin/bash
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        #echo Package manager: ${osInfo[$f]}
+        pman=${osInfo[$f]}
+    fi
+done
+
+# check if jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+    echo "Installing jq"
+
+    # check for sudo, install
+    if [ -x "$(command -v sudo)" ]; then
+        sudo $pman install jq -y
+    else
+        $pman install jq -y
+    fi
+else
+    echo "jq already installed"
+fi
+
 streamName=$(jq -r '."Stream Name"' ./settings.json)
 outputType=$(jq -r '."Output Type"' ./settings.json)
 address=$(jq -r '."Address"' ./settings.json)
