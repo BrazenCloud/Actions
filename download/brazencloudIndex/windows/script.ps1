f0ab7d4c8f52fb6c4540bdde902831d7d621975d
Anthony Howell <anthony@howell-it.com>
2022-10-03 08:43:59 -0700

    adding index clear

--- download/brazencloudIndex/windows/script.ps1
+++ download/brazencloudIndex/windows/script.ps1
@@ -1,3 +1,48 @@
+Function Remove-BcDatastoreQuery2 {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$IndexName,
+        [string]$GroupId = (Get-BcAuthenticationCurrentUser).HomeContainerId,
+        #example:  @{query=@{match=@{type='agentInstall'}}}
+        [Parameter(Mandatory)]
+        [hashtable]$Query
+    )
+    $splat = @{
+        Method  = 'Delete'
+        Uri     = "https://$($env:BrazenCloudDomain)/api/v2/datastore/$IndexName/delete"
+        Body    = @{
+            deleteQuery = $Query
+            groupId     = $GroupId
+        } | ConvertTo-Json -Depth 10
+        Headers = @{
+            Accept         = 'application/json'
+            'Content-Type' = 'application/json'
+            Authorization  = "Session $($env:BrazenCloudSessionToken)"
+        }
+    }
+    Invoke-RestMethod @splat
+}
+Function Get-BrazenCloudDaemonToken {
+    # outputs the session token
+    [OutputType([System.String])]
+    [CmdletBinding()]
+    param (
+        [string]$aToken,
+        [string]$Domain
+    )
+    $authResponse = Invoke-WebRequest -UseBasicParsing -Uri "$Domain/api/v2/auth/ping" -Headers @{
+        Authorization = "Daemon $aToken"
+    }
+    
+    if ($authResponse.Headers.Authorization -like 'Session *') {
+        return (($authResponse.Headers.Authorization | Select-Object -First 1) -split ' ')[1]
+    } else {
+        Throw 'Failed auth'
+        exit 1
+    }
+}
+
 $settings = Get-Content .\settings.json | ConvertFrom-Json
 $settings
 
@@ -9,25 +54,14 @@ $outFolder = '.\out'
 if (-not (Test-Path $outFolder)) {
     New-Item $outFolder -ItemType Directory
 }
-& .\windows\runway.exe -N -S $settings.host download --directory $outFolder
+& ..\..\..\runway -N -S $settings.host download --directory $outFolder
 
 $zips = Get-ChildItem $outFolder -Filter *.zip -Recurse -Verbose
 $zips | ForEach-Object {
     Expand-Archive $_.fullName -DestinationPath $outFolder
 }
 
-$authResponse = Invoke-WebRequest -UseBasicParsing -Uri "$($settings.host)/api/v2/auth/ping" -Headers @{
-    Authorization = "Daemon $($settings.atoken)"
-}
-
-if ($authResponse.Headers.Authorization -like 'Session *') {
-    $auth = $authResponse.Headers.Authorization | Select-Object -First 1
-} else {
-    Throw 'Failed auth'
-    exit 1
-}
-
-Write-Host "auth: $auth"
+$auth = Get-BrazenCloudDaemonToken -aToken $settings.atoken -Domain $settings.host
 
 $headers = @{
     Authorization  = $auth
@@ -41,6 +75,10 @@ $group = Invoke-RestMethod -Uri "$($settings.host)/api/v2/jobs/parent-group/$($s
 }
 
 Write-Host "Group: $group"
+
+if ($settings.'Clear Index'.ToString() -eq 'true') {
+    Remove-BcDatastoreQuery2 -GroupId $group -IndexName $settings.'Index Name' -Query @{query = @{match_all = @{} } } -ErrorAction SilentlyContinue | Out-Null
+}
 
 # Upload results
 Get-ChildItem $outFolder\* -Include *.json, *.ndjson | ForEach-Object {
