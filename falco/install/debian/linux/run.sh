152119fe38a4530547ee29842d0d09d79ba6ff69
Anthony Howell <anthony@howell-it.com>
2022-10-10 10:40:12 -0700

    adding falco install actions

---
+++ falco/install/debian/linux/run.sh
@@ -0,0 +1,11 @@
+# https://falco.org/docs/getting-started/installation/#debian
+# Trust the falcosecurity GPG key, configure the apt repository, and update the package list:
+curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
+echo "deb https://download.falco.org/packages/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
+apt-get update -y
+
+# Install kernel headers:
+apt-get -y install linux-headers-$(uname -r)
+
+# Install Falco:
+apt-get install -y falco
---
+++ falco/install/rhel/linux/run.sh
@@ -0,0 +1,13 @@
+# Trust the falcosecurity GPG key and configure the yum repository:
+rpm --import https://falco.org/repo/falcosecurity-3672BA8F.asc
+curl -s -o /etc/yum.repos.d/falcosecurity.repo https://falco.org/repo/falcosecurity-rpm.repo
+
+# Note — The following command is required only if DKMS and make are not available in the distribution. You can verify if DKMS is available using yum list make dkms. If necessary install it using: yum install epel-release (or amazon-linux-extras install epel in case of amzn2), then yum install make dkms.
+
+# Install kernel headers:
+yum -y install kernel-devel-$(uname -r)
+
+# Note — If the package was not found by the above command, you might need to run yum distro-sync in order to fix it. Rebooting the system may be required.
+
+# Install Falco:
+yum -y install falco
