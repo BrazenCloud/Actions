66286038b98078ee27ebffa00e4a3635293a6e58
Anthony Howell <anthony@howell-it.com>
2022-10-11 10:57:07 -0700

    adding params for installing or start service

--- falco/install/rhel/linux/run.sh
+++ falco/install/rhel/linux/run.sh
@@ -1,3 +1,39 @@
+#!/bin/bash
+
+# check for current package manager
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        #echo Package manager: ${osInfo[$f]}
+        pman=${osInfo[$f]}
+    fi
+done
+
+# check if jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+    echo "Installing jq"
+
+    # check for sudo, install
+    if [ -x "$(command -v sudo)" ]; then
+        sudo $pman install jq -y
+    else
+        $pman install jq -y
+    fi
+else
+    echo "jq already installed"
+fi
+
+installService=$(jq -r '."Install Service"' ./settings.json)
+startService=$(jq -r '."Start Service"' ./settings.json)
+
 # Trust the falcosecurity GPG key and configure the yum repository:
 rpm --import https://falco.org/repo/falcosecurity-3672BA8F.asc
 curl -s -o /etc/yum.repos.d/falcosecurity.repo https://falco.org/repo/falcosecurity-rpm.repo
@@ -12,9 +48,13 @@ yum -y install kernel-devel-$(uname -r)
 # Install Falco:
 yum -y install falco
 
-# install driver
-falco-driver-loader
+if $installService == 'true'; then
+    # install driver
+    falco-driver-loader
 
-# start service
-systemctl enable falco
-systemctl start falco
+    if $startService == 'true'; then
+        # start service
+        systemctl enable falco
+        systemctl start falco
+    fi
+fi
