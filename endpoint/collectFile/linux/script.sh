4ef88c301e9eacaf903528845a7e5a658b720f55
Anthony Howell <anthony@howell-it.com>
2022-10-25 14:33:21 -0700

    adding linux support

---
+++ endpoint/collectFile/linux/script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# check for current package manager
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        #echo Package manager: ${osInfo[$f]}
+        pman=${osInfo[$f]}
+    fi
+done
+
+# check if jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+    echo "Installing jq"
+    # check for sudo, install
+    if [ -x "$(command -v sudo)" ]; then
+        sudo $pman install jq -y
+    else
+        $pman install jq -y
+    fi
+else
+    echo "jq already installed"
+fi
+
+filePath=$(jq -r '."File Path"' ./settings.json)
+
+if [[ -f $filePath ]]; then
+    cp $filePath ./results
+else
+    echo "'$filePath' is not a valid path."
+fi
