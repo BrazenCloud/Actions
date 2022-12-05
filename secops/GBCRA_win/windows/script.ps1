45b9d31cfdc642bca7d8f04876f032ba462d63c3
Anthony Howell <anthony@howell-it.com>
2022-12-05 14:30:06 -0800

    performance improvements, output

--- secops/GBCRA_win/windows/script.ps1
+++ secops/GBCRA_win/windows/script.ps1
@@ -8,8 +8,8 @@
 # Correlate low frequency process commands to suspicious activities; and improper users by host to activities.
 
 #Clear-Host
-$localpath = "C:\secaudit" # This is the location where the output files will drop at runtime
-$outpath = "c:\windows\temp"
+$localpath = ".\results" # This is the location where the output files will drop at runtime
+#$outpath = ".\results"
 
 $logtime = (Get-Date -UFormat %s)
 
@@ -31,27 +31,35 @@ foreach ($service in (Get-CimInstance -class win32_service)) {
 
 
 # NetProcMon
+Write-Host "Auditing process data..."
 $WP = @{}
 Get-WmiObject Win32_Process | ForEach-Object { $WP[$_.ProcessID] = $_ }
-Get-NetTCPConnection |
-    Select-Object -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, State,
-    @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } }, 
-    @{Name = 'AuditDate'; Expression = { $auditDate } }, 
-    @{Name = 'PID'; Expression = { $_.OwningProcess } },
-    @{Name = 'Process'; Expression = { ($procHt[$_.OwningProcess]).Name } },
-    @{Name = 'UserName'; Expression = { ($procHt[$_.OwningProcess]).UserName } },
-    @{Name = 'ServiceName'; Expression = { ($servHt[$_.OwningProcess]).Name } },
-    @{Name = 'ServiceStartType'; Expression = { ($servHt[$_.OwningProcess]).StartMode } },
-    @{Name = 'Path'; Expression = { ($procHt[$_.OwningProcess]).Path } }, 
-    @{Name = 'SHA1'; Expression = { (Get-FileHash ($procHt[$_.OwningProcess]).Path -Algorithm SHA1 | Select-Object -ExpandProperty Hash) } },
-    @{Name = 'CommandLine'; Expression = { $WP[[UInt32]$_.OwningProcess].CommandLine } }, 
-    @{Name = 'Connected'; Expression = { (Get-Date -UFormat %s $_.CreationTime) } } |
-        Select-Object Computername, AuditDate, UserName, PID, Process, ServiceName, Path, 
-        ServiceStartType, SHA1, CommandLine, Connected, State, LocalAddress, LocalPort, RemoteAddress, RemotePort |
-            Where-Object Process -NotLike 'Idle' | 
-                Export-Csv -Path $localpath\"$env:computername"-activecomms.csv -Append -Encoding UTF8 -NoTypeInformation $ErrorActionPreference
+Get-NetTCPConnection | ForEach-Object {
+    [pscustomobject]@{
+        LocalAddress     = $_.LocalAddress
+        LocalPort        = $_.LocalPort
+        RemoteAddress    = $_.RemoteAddress
+        RemotePort       = $_.RemotePort
+        State            = $_.State
+        Computername     = $env:COMPUTERNAME
+        AuditDate        = $auditDate
+        PID              = $_.OwningProcess
+        Process          = ($procHt[$_.OwningProcess]).Name
+        UserName         = ($procHt[$_.OwningProcess]).UserName
+        ServiceName      = ($servHt[$_.OwningProcess]).Name
+        ServiceStartType = ($servHt[$_.OwningProcess]).StartMode
+        Path             = ($procHt[$_.OwningProcess]).Path
+        SHA1             = $(Get-FileHash ($procHt[$_.OwningProcess]).Path -Algorithm SHA1).Hash
+        CommandLine      = $WP[[UInt32]$_.OwningProcess].CommandLine
+        Connected        = (Get-Date -UFormat %s $_.CreationTime)
+    }
+} | Select-Object Computername, AuditDate, UserName, PID, Process, ServiceName, Path, 
+ServiceStartType, SHA1, CommandLine, Connected, State, LocalAddress, LocalPort, RemoteAddress, RemotePort |
+    Where-Object Process -NotLike 'Idle' | 
+        Export-Csv -Path $localpath\"$env:computername"-activecomms.csv -Append -Encoding UTF8 -NoTypeInformation $ErrorActionPreference
 	
 # ServiceBinaries
+Write-Host 'Auditing service data...'
 Get-Service | ForEach-Object {
     $binaryPath = $_.BinaryPathName
     if (-not $binaryPath) {
@@ -72,14 +80,14 @@ Get-Service | ForEach-Object {
     }
     $FileVersionInfo = if ($binaryPath) { (Get-Item -LiteralPath $binaryPath).VersionInfo }
     [pscustomobject] @{
-        Name            = $_.Name
-        BinaryPath      = if ($binaryPath) { $binaryPath } else { '(n/a)'; Write-Error "Failed to determine binary path for service '$($_.Name)'. Try running as admin." }
-        ProductName     = $FileVersionInfo.ProductName
-        FileDescription = $FileVersionInfo.FileDescription
-        CompanyName     = $FileVersionInfo.CompanyName
-        FileVersion     = $FileVersionInfo.FileVersion
-        ProductVersion  = $FileVersionInfo.ProductVersion
-        SHA1            = (Get-FileHash $binaryPath -Algorithm SHA1 | Select-Object -ExpandProperty Hash)
+        Name            = $($_.Name)
+        BinaryPath      = $(if ($binaryPath) { $binaryPath } else { '(n/a)'; Write-Error "Failed to determine binary path for service '$($_.Name)'. Try running as admin." })
+        ProductName     = $($FileVersionInfo.ProductName)
+        FileDescription = $($FileVersionInfo.FileDescription)
+        CompanyName     = $($FileVersionInfo.CompanyName)
+        FileVersion     = $($FileVersionInfo.FileVersion)
+        ProductVersion  = $($FileVersionInfo.ProductVersion)
+        SHA1            = $(Get-FileHash $binaryPath -Algorithm SHA1 | Select-Object -ExpandProperty Hash)
     }
 } |
     Select-Object @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } }, 
@@ -88,6 +96,7 @@ Get-Service | ForEach-Object {
         Export-Csv -Path $localpath\"$env:computername"-servicebinaries.csv -Append -Encoding UTF8 -NoTypeInformation $ErrorActionPreference
 
 # NICSettings
+Write-Host 'Auditing NIC data...'
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration | 
     Select-Object @{Label = "Computername"; Expression = { $_.__SERVER } }, 
     @{Name = 'AuditDate'; Expression = { Get-Date -UFormat %s } }, 
@@ -102,6 +111,7 @@ Get-WmiObject -Class Win32_NetworkAdapterConfiguration |
         Export-Csv -Path $localpath\"$env:computername"-nic.csv -Encoding UTF8 -NoTypeInformation 
 	
 # DNSCache
+Write-Host 'Auditing DNS data...'
 Invoke-Command -ScriptBlock {
     ipconfig /displaydns | 
         Select-Object -Unique @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } }, 
@@ -112,6 +122,7 @@ Invoke-Command -ScriptBlock {
             Export-Csv -Path $localpath\"$env:computername"-dnscache.csv -Encoding UTF8 -NoTypeInformation
 
 # OSInfo
+Write-Host 'Auditing OS data...'
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" | 
     Select-Object @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } },
     @{Name = 'AuditDate'; Expression = { Get-Date -UFormat %s } }, 
@@ -123,38 +134,42 @@ Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" |
         Export-Csv -Path $localpath\"$env:computername"-osinfo.csv -Encoding UTF8 -NoTypeInformation
 
 # Missing Windows Patches
+Write-Host 'Auditing Windows updates...'
 $Patches = @(
     $MUS = New-Object -com Microsoft.Update.Session
     $Usearch = $MUS.CreateUpdateSearcher()
     $Usresult = $Usearch.Search("IsInstalled=0 and Type='Software'")
     ForEach ($update in $Usresult.Updates) {
-        $update | Select-Object @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } },
-        @{Name = 'AuditDate'; Expression = { Get-Date -UFormat %s } },
-        @{Name = 'Patch'; Expression = { $Update.Title } }
+        [pscustomobject]@{
+            ComputerName = $env:COMPUTERNAME
+            AuditDate    = (Get-Date -UFormat %s)
+            Patch        = $Update.Title
+        }
     }
     $ErrorActionPreference )  
 $Patches | ConvertTo-Csv -NoTypeInformation |
     Out-File $localpath\"$env:computername"-missingpatches.csv -Encoding UTF8
 
 # LOCALUsersGroups
+$ADS_UF_ACCOUNTDISABLE = 0x000002
+Write-Host 'Auditing local groups...'
 $adsi = [ADSI]"WinNT://$env:COMPUTERNAME"
 $adsi.Children | Where-Object { $_.SchemaClassName -eq 'user' } | ForEach-Object {
     $groups = $_.Groups() | ForEach-Object {
         $_.GetType().InvokeMember('Name', 'GetProperty', $null, $_, $null)
     }
-    $_ | Select-Object @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } },
-    @{Name = 'AuditDate'; Expression = { Get-Date -UFormat %s } },
-    @{n = 'UserName'; e = { $_.Name } },
-    @{n = 'LastLogin'; e = { $_.LastLogin | Get-Date -UFormat %s } }, 
-    @{name = "Enabled"; Expression = {
-            if ($_.psbase.properties.item("userflags").value -band $ADS_UF_ACCOUNTDISABLE) {
-                $False 
-            } else { $True } }
-    },
-    @{n = 'Groups'; e = { $groups -join ';' } }
+    [pscustomobject]@{
+        Computername = $env:COMPUTERNAME
+        AuditDate    = $(Get-Date -UFormat %s)
+        UserName     = $($_.Name)
+        LastLogin    = $(Get-Date $_.LastLogin -UFormat %s)
+        Enabled      = $(-not ($_.psbase.properties.item("userflags").value -band $ADS_UF_ACCOUNTDISABLE))
+        Groups       = $($groups -join ';')
+    }
 } | Export-Csv -Path $localpath\"$env:computername"-localusergroups.csv -Encoding UTF8 -NoTypeInformation $ErrorActionPreference
 
 # ProfileSIDs
+Write-Host 'Auditing profiles...'
 Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*" |
     Select-Object @{Name = 'Computername'; Expression = { $env:COMPUTERNAME } },
     @{Name = 'AuditDate'; Expression = { Get-Date -UFormat %s } }, 
@@ -163,56 +178,50 @@ Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Profi
         Export-Csv -Path $localpath\"$env:computername"-profilesids.csv -Encoding UTF8 -NoTypeInformation $ErrorActionPreference 
 
 # SCHEDULEDTasks
+Write-Host 'AUditing scheduled tasks...'
 $sched = New-Object -Com "Schedule.Service"
 $sched.Connect()
-$out = @()
-$sched.GetFolder("\").GetTasks(0) | ForEach-Object {
+$out = $sched.GetFolder("\").GetTasks(0) | ForEach-Object {
     $xml = [xml]$_.xml
-    $out += New-Object psobject -Property @{
-        "ComputerName" = $env:COMPUTERNAME 
-        "Name"         = $_.Name
-        "Status"       = switch ($_.State) { 0 { "Unknown" } 1 { "Disabled" } 2 { "Queued" } 3 { "Ready" } 4 { "Running" } }
-        "LastRunTime"  = $_.LastRunTime 
-        "NextRunTime"  = $_.NextRunTime
-        "Actions"      = ($xml.Task.Actions.Exec | ForEach-Object { "$($_.Command) $($_.Arguments)" }) -join "`n"
-        "Enabled"      = $xml.task.settings.enabled
-        "Author"       = $xml.task.principals.Principal.UserID
-        "Description"  = $xml.task.registrationInfo.Description
-        "RunAs"        = $xml.task.principals.principal.userid
-        "Created"      = $xml.Task.RegistrationInfo.Date
+    [pscustomobject]@{
+        ComputerName = $($env:COMPUTERNAME )
+        AuditDate    = $(Get-Date -UFormat %s)
+        Name         = $($_.Name)
+        Status       = $(switch ($_.State) { 0 { "Unknown" } 1 { "Disabled" } 2 { "Queued" } 3 { "Ready" } 4 { "Running" } })
+        LastRunTime  = $(Get-Date $_.LastRunTime -UFormat %s)
+        NextRunTime  = $(Get-Date $_.NextRunTime -UFormat %s)
+        Actions      = $(($xml.Task.Actions.Exec | ForEach-Object { "$($_.Command) $($_.Arguments)" }) -join "`n")
+        Enabled      = $($xml.task.settings.enabled)
+        Author       = $($xml.task.principals.Principal.UserID)
+        Description  = $($xml.task.registrationInfo.Description)
+        RunAs        = $($xml.task.principals.principal.userid)
+        Created      = $(Get-Date $xml.Task.RegistrationInfo.Date -UFormat %s)
     }
 }
-$out | 
-    Select-Object Computername, 
-    @{Name = 'AuditDate'; Expression = { Get-Date -UFormat %s } }, 
-    Name, 
-    Status, 
-    @{ Label = "LastRunTime"; Expression = { $_.LastRunTime | Get-Date -UFormat %s } },
-    @{ Label = "NextRunTime"; Expression = { $_.NextRunTime | Get-Date -UFormat %s } },
-    Actions, 
-    Enabled, 
-    Author, 
-    Description, 
-    RunAs, 
-    @{ Label = "Created"; Expression = { $_.Created | Get-Date -UFormat %s } } |
-        Export-Csv -Path $localpath\"$env:computername"-tasks.csv -Encoding UTF8 -NoTypeInformation
+$out | Export-Csv -Path $localpath\"$env:computername"-tasks.csv -Encoding UTF8 -NoTypeInformation
 
 ## CLEANUP
-# ZIP Results
+<# For the BrazenCloud version, no need to zip
+ZIP Results
+Write-Host 'Cleaning up...'
 $zip = $localpath + "\" + $env:Computername + "-" + $logtime + ".zip" 
-New-Item $zip -ItemType file
-$shellApplication = New-Object -com shell.application
-$zipPackage = $shellApplication.NameSpace($zip)
-$files = Get-ChildItem -Path $localpath\* -Exclude "*.zip" -Recurse
-Start-Sleep -Milliseconds 1000
-foreach ($file in $files) { 
-    $zipPackage.CopyHere($file.FullName)
+if ($PSVersionTable.PSVersion.Major -ge 5) {
+    Compress-Archive $localPath\*.csv -DestinationPath $zip
+} else {
+    New-Item $zip -ItemType file
+    $shellApplication = New-Object -com shell.application
+    $zipPackage = $shellApplication.NameSpace($zip)
+    $files = Get-ChildItem -Path $localpath\* -Exclude "*.zip" -Recurse
+    Start-Sleep -Milliseconds 1000
+    foreach ($file in $files) { 
+        $zipPackage.CopyHere($file.FullName)
+        Start-Sleep -Milliseconds 1000
+    }
     Start-Sleep -Milliseconds 1000
 }
-Start-Sleep -Milliseconds 1000
 # MOVE zip file to the network share
 Move-Item $localpath\*.zip $outpath -Force
 # REMOVE Files and Folder
 Remove-Item $localpath -Recurse -Force
-
+#>
 # End of CBA Script
